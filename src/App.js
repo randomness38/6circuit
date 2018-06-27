fd3e92372775e8c06883acc79393b7d215e978eb
randomness38 <randomness.jay@gmail.com>
2018-06-27 11:44:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../src/sass/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
